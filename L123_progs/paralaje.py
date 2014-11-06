5061b37f2e07aaa6eeee653a417537a7b0f40179
cmateu <cmateu@astrosen.unam.mx>
2014-11-06 16:47:35 +0100

    paralaje.py fixed

--- L123_progs/paralaje.py
+++ L123_progs/paralaje.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy
 import pylab as plt
 
-scipy.random.seed(seed=12)
+#scipy.random.seed(seed=12)
 
 def estrella(N,true_par,err_pi):
   pi=scipy.random.normal(loc=true_par,scale=err_pi,size=N)
@@ -21,28 +21,30 @@ for err_pii in [0.005,0.1]:
  #for N in [200,]:
   pi=np.append(pi,estrella(N-pi.size,true_par,err_pii))
   err_pi=err_pii*np.ones(N)
-  plt.subplot(2,2,i+1)
-  plt.hist(pi,histtype='step',normed=True,lw=3.)
-  plt.xlim(-0.3,0.3)
-  if i==2: plt.xlabel('$\pi$')
-  plt.ylabel('$N(\pi)$')
-  if N==Nmucho: plt.title('$\langle 1/\pi_k \\rangle=%.2f$, $1/\langle\pi_k \\rangle=%.2f$' % (np.mean(1./pi),1./(np.mean(pi))))
+  ax=plt.subplot(2,2,i+1)
+  ax.hist(pi,histtype='step',normed=True,lw=3.)
+  ax.set_xlim(-0.3,0.3)
+  if ax.is_last_row(): ax.set_xlabel('$\pi$')
+  if ax.is_first_col(): ax.set_ylabel('$N(\pi)$')
+  if ax.is_first_row(): 
+   ax.set_title('$\langle 1/\pi_k \\rangle=%.2f$, $1/\langle\pi_k \\rangle=%.2f$' % (np.mean(1./pi),1./(np.mean(pi))))
   
   D_v=np.linspace(0.01,100.,100)
   lnlikelihood=np.array([-2*np.log(D) -np.sum(((pi-1./D)/(np.sqrt(2)*err_pi))**2) for D in D_v])
   posterior=np.exp(lnlikelihood)
   posterior=posterior/np.max(posterior)
-  plt.subplot(2,2,i+2)
-  plt.plot(D_v,posterior,'-',label='N=%d' % (N))
+  #----
+  ax=plt.subplot(2,2,i+2)
+  ax.plot(D_v,posterior,'-',label='N=%d' % (N))
   meanpi=np.sum(pi/err_pi**2)
   wpi=np.sum(1/err_pi**2)
   Do=-meanpi/4. + (1/4.)*np.sqrt(meanpi**2+8*wpi )
-  plt.axvline(x=Do,ls='--',color='k')
+  ax.axvline(x=Do,ls='--',color='k')
   #if N==Nmucho: plt.title('$D_o=%.2f$ (N=%d)' % (D_v[np.argmax(posterior)],N))
   if N==Nmucho: plt.title('$D_o=%.2f$ (N=%d)' % (Do,N))
   if i==2: plt.xlabel('D')
-  plt.ylabel('$p(D|\{\pi_i\})$')
-  if i==0: plt.legend()
+  ax.set_ylabel('$p(D|\{\pi_i\})$')
+  if ax.is_first_row() and ax.is_first_col(): plt.legend()
  i=i+2
 plt.savefig('par.png')
 plt.show()
