e539478abd56b6bd84b0af8798c94c77752b044d
cmateu <cmateu@astrosen.unam.mx>
2016-10-25 17:22:51 +0200

    programs updated

--- L123_progs/moneda_ex.py
+++ L123_progs/moneda_ex.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import scipy
+import scipy.interpolate
 import pylab as plt
 import sys
 import argparse
@@ -20,6 +21,52 @@ def select_prior(Nprior):
 
   return (prior,col)
  
+def Likelihood(H,N,Nh):
+   return (H**Nh)*((1-H)**(N-Nh))
+
+#-----------------------------------------------------------------------------------------------------
+def get_1d_confidence_intervals(xori,zori):
+
+  #Copy
+  x=xori.copy()
+  #Normalize z
+  z=zori.copy()/max(zori)
+
+  zlevels=np.arange(0.,1.,0.0001)
+  xs=np.linspace(np.min(x),np.max(x),500)
+
+  Linear=scipy.interpolate.interp1d(x, z,kind='linear')
+  zint=Linear(xs)
+
+  I=zint.sum()
+
+  Psum=np.array([])
+  for level in zlevels:
+    Psum=np.append(Psum,np.sum(zint[zint>level])/I)
+
+  #Standard values for 1-sigma,2-sigma,3-sigma (gaussian) confidence intervals
+  sigma_levels=np.array([0.683,0.954,0.997])
+
+  tol=0.05
+  sigma_Ps=np.array([])
+  xinf=xsup=np.array([])
+  for slevels in sigma_levels:
+    if zlevels[abs(Psum-slevels)<tol].any():
+      sigma_Ps=np.append(sigma_Ps,max(zlevels[abs(Psum-slevels)<tol]))
+      mask=zint>sigma_Ps[-1]
+      xinf, xsup = np.append(xinf,min(xs[mask])), np.append(xsup,max(xs[mask]))
+    elif xs[zint>tol].any():
+      sigma_Ps=np.append(sigma_Ps,0.)
+      xinf, xsup = np.append(xinf,min(xs[zint>tol])), np.append(xsup,max(xs[zint>tol]))
+    else:
+      xinf, xsup = np.zeros(3), np.zeros(3)
+
+  xmax=x[np.argmax(z)]
+
+  return (sigma_levels,sigma_Ps,xinf,xsup,xmax)
+
+
+
 #===================================================================================================
 
 __what__= sys.argv[0]+": This program illustrates the coin example for some user-defined inputs."
@@ -32,6 +79,7 @@ parser.add_argument("-p","--priorkeys", help="Select Prior Key [1=Uniform,2=Gaus
 parser.add_argument("-o","--outplot", help="Output plot filename", nargs=1,default='moneda.png')
 parser.add_argument("-f","--figsave", help="Save figure",action='store_true',default=False)
 parser.add_argument("-m","--median", help="Show median",action='store_true',default=False)
+parser.add_argument("-ci", help="Show confidence intervals",action='store_true',default=False)
 
 args = parser.parse_args()
 Nprior_l=args.priorkeys
@@ -48,8 +96,9 @@ for N in [0,Ntot]:
   #Toss coin and save the coin tosses appended to the previous ones
   #------------
   #Likelihood 
-  if N>0: likelihood=(H**Nh)*((1-H)**(N-Nh))
-  else: likelihood=(H**0)*((1-H)**(N-0))
+  if N==0: likelihood=Likelihood(H,N,0)
+  else: likelihood=Likelihood(H,N,Nh)
+
   ax=fig.add_subplot(1,2,i)
   #Compute and plot posterior for different priors
   for Nprior in Nprior_l:
@@ -59,7 +108,20 @@ for N in [0,Ntot]:
     Hbest=H[posterior.argmax()]
     Psum=posterior.cumsum()/posterior.sum()
     Hmedian=H[Psum<=0.5][-1]
-    if args.median: ax.axvline(x=Hmedian,ls='--',lw=1,c=pcol)
+    if args.median and N>0: ax.axvline(x=Hmedian,ls='-',lw=2,c=pcol,label='median')
+
+    #Confidence intervals
+    if N>0:
+     ci_tuple=get_1d_confidence_intervals(H,posterior)
+     lss=['--',';',':']
+     fs=[3.,2.,1.]
+     posterior_norm=posterior/np.max(posterior)
+     if args.ci:
+      for ii in range(3):
+        #ax.axvline(ci_tuple[2][ii],ls=lss[0],lw=fs[ii],c=pcol)
+        #ax.axvline(ci_tuple[3][ii],ls=lss[0],lw=fs[ii],c=pcol)
+        mask_ci=(H>=ci_tuple[2][ii]) & (H<=ci_tuple[3][ii])
+        ax.fill_between(H[mask_ci],0*posterior_norm[mask_ci],posterior_norm[mask_ci],color=pcol,alpha=0.2)
 
   #-------Format stuff-----------------------------------------------
   if i==1: print 'N=%-5d, Nh=%-d, Hmode=%5.3f, Hmedian=%5.3f' % (N,Nh,Hbest,Hmedian)
@@ -72,6 +134,7 @@ for N in [0,Ntot]:
   ax.grid(which='both')   
   if not ax.is_last_row(): ax.xaxis.set_ticklabels([])
   if not ax.is_first_col(): ax.yaxis.set_ticklabels([])
+
 
 #-------------------------Save plot---------------------
 if args.figsave: plt.savefig(args.outplot[0])
